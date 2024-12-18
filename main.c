ba09274767a14cb82d75d6fdd2042edc5a274d24
Patrick Queiroz <patrick.sq52@gmail.com>
2024-12-18 20:15:44 -0300

    Iniciando o projeto de conversão de valores

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Função que vai exibir o menu principal
+
+void menu() {
+    printf("========== Conversão para Profissionais ==========\n");
+
+    // Código a ser implementado
+    
+    printf("================ fim do menu ======================\n");
+}
+
+// Função principal
+
+int main() {
+    
+    menu(); // Chama a função menu para exibir o menu principal
+
+    return 0;
+}
