1693fbb43cb0822bd3df798f643921c79de2e3ff
Patrick Queiroz <patrick.sq52@gmail.com>
2024-12-19 15:22:28 -0300

    atualizando código pra testar o merge

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Funções para chamar cada módulo a ser implementado.
+
+void conversoes_area(){
+    printf("A ser implementado por João");
+};
+
+void conversoes_volume(){
+    printf("A ser implementado por Ana");
+};
+
+void conversoes_comprimento(){
+    printf("A ser implementado por Garcia");
+}
+
+void conversoes_digitais(){
+    printf("A ser implementado por Josimar");    
+}
+
+void calculo_materiais(){
+    printf("A ser implementado por Alex");
+};
+
+void conversoes_tempo(){
+    printf("A ser implementado por Andre ");
+}
+
+// Função que vai exibir o menu principal
+
+void menu() {
+    printf("========== Conversão para Profissionais ==========\n");
+
+    // Código a ser implementado pelo Heitor
+    
+    printf("================ fim do menu ======================\n");
+}
+
+// Função principal - A sr implementado por Patrick
+
+int main() {
+    
+    menu(); // Chama a função menu para exibir o menu principal
+
+    return 0;
+};
