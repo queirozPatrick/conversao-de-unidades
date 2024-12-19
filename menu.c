e77c018264df77c0c7a1c109995537e9d1aab898
Heitor Lemos <heitorlemoss@gmail.com>
2024-12-19 11:21:10 -0300

    Adicionando o código escopo para o menu do projeto

---
+++ menu.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+void menu() { 
+    printf("========== Conversão para Profissionais ==========\n"); 
+    printf("1. Conversões de Área (por João)\n"); 
+    printf("2. Conversões de Volume (por Ana)\n"); 
+    printf("3. Conversões de Comprimento (por Garcia)\n"); 
+    printf("4. Conversões Digitais (por Josimar)\n"); 
+    printf("5. Cálculo de Materiais (por Alex)\n"); 
+    printf("6. Conversões de Tempo (por Andre)\n"); 
+    printf("7. Sair\n"); 
+    printf("================ fim do menu ======================\n"); 
+    
+} 
+
+int main(){
+    menu();
+
+    return 0;
+}
+    
